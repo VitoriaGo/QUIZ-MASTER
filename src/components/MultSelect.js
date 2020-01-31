d8c51697e933d90186c2e003d7d8c36e77b9aa41
VITORIA GOMES <vitoria.g.barreto@gmail.com>
2020-01-31 11:26:24 -0300

    --- Auto-Git Commit ---

--- src/components/MultSelect.js
+++ src/components/MultSelect.js
@@ -4,9 +4,8 @@ import TextBox from "./MetaSelect"
 function MultSelect (props){
   let AnsArray = []
   let Question = <h1>{props.question.question}</h1>
-  let Answer = props.question.answers.map(
-    (string) => {if (string === "Outros:") {<button onClick={() => {TextBox}}>{string}oi</button>}
-                else { <button onClick={() => {AnsArray.push(string); console.log(AnsArray)}}>{string}</button>)}}
+  let Answer = props.question.answers.map((string) => { if (string === "Outros:") return(<button onClick={() => {TextBox}}>{string}oi</button>)
+                else  return(<button onClick={() => {AnsArray.push(string); console.log(AnsArray)}}>{string}</button>)})
   return (
     <div className="multSelect">
     {Question}
